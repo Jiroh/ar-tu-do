73e78ff1ef1cd14ffccdb591c95e10dfdc242204
Robin Thunig <robin.thunig@tu-dortmund.de>
2019-12-01 21:11:59 +0100

    feat: add service for braking with a given distance

--- ros_ws/src/car_control_test/include/drive_straight.h
+++ ros_ws/src/car_control_test/include/drive_straight.h
@@ -2,7 +2,9 @@
 
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <nav_msgs/Odometry.h>
 #include "car_control_test/Drive.h"
+#include "car_control_test/TargetSpeed.h"
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-equal"
@@ -10,6 +12,7 @@
 #pragma GCC diagnostic pop
 
 constexpr const char* TOPIC_FOCBOX_SPEED = "/commands/motor/speed";
+constexpr const char* TOPIC_ODOM = "/odom";
 
 
 /**
@@ -22,11 +25,21 @@ class StraightDrive
     StraightDrive();
     void publishSpeed(double speed);
     float m_speed = 0;
+    int count = 0;
 
     private:
     ros::NodeHandle m_node_handle;
     ros::Publisher m_straightdrive_publisher;
     ros::ServiceServer m_drive_with_speed;
+    ros::ServiceServer m_speed_after_distance;
+    ros::Subscriber m_odom;
+
+    float current_speed;
 
     bool drive_with_speed(car_control_test::Drive::Request &request, car_control_test::Drive::Response &response);
+
+    bool speedAfterDistanceService(car_control_test::TargetSpeed::Request &request, car_control_test::TargetSpeed::Response &response);
+    float calcBreakingDistance(float current_speed, float target_speed, float distance, float acceleration);
+
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& odom);
 };
--- ros_ws/src/car_control_test/src/drive_straight.cpp
+++ ros_ws/src/car_control_test/src/drive_straight.cpp
@@ -2,9 +2,11 @@
 
 StraightDrive::StraightDrive()
 {
-    
     this->m_straightdrive_publisher = m_node_handle.advertise<std_msgs::Float64>(TOPIC_FOCBOX_SPEED, 1);
     this->m_drive_with_speed = m_node_handle.advertiseService("drive_with_speed", &StraightDrive::drive_with_speed, this);
+    this->m_speed_after_distance = m_node_handle.advertiseService("speed_after_distance", &StraightDrive::speedAfterDistanceService, this);
+    this->m_odom =
+        this->m_node_handle.subscribe(TOPIC_ODOM, 1, &StraightDrive::odomCallback, this);
 }
 
 void StraightDrive::publishSpeed(double speed)
@@ -20,6 +22,32 @@ bool StraightDrive::drive_with_speed(car_control_test::Drive::Request &request, 
     m_speed = request.speed;
     std::cout<< "Forwards" << std::endl;
     return true;
+}
+
+/*
+current_speed in m/s
+target_speed in m/s
+distance in m
+acceleration in m/s^2
+*/
+float StraightDrive::calcBreakingDistance(float current_speed, float target_speed, float distance, float acceleration) 
+{
+    return (distance/2) + ((current_speed*current_speed-target_speed*target_speed)/(4*acceleration));
+}
+
+bool StraightDrive::speedAfterDistanceService(car_control_test::TargetSpeed::Request &request, car_control_test::TargetSpeed::Response &response)
+{
+
+    float breaking_distance = calcBreakingDistance(current_speed, request.target_speed, request.distance, 10.0);
+    response.breaking_distance = breaking_distance;
+    response.braking_possible = breaking_distance < request.distance;
+    response.current_speed = current_speed;
+    return true;
+}
+
+void StraightDrive::odomCallback(const nav_msgs::Odometry::ConstPtr& odom) 
+{
+    current_speed = odom->twist.twist.linear.x;
 }
 
 int main(int argc, char** argv)
