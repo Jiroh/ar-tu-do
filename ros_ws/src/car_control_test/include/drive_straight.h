21086884b89635664dac9bf4361f15a0f22933f9
Robin Thunig <robin.thunig@tu-dortmund.de>
2019-12-17 22:40:50 +0100

    feat: use max possible velocity as target velocity

--- ros_ws/src/car_control_test/include/drive_straight.h
+++ ros_ws/src/car_control_test/include/drive_straight.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "../../simulation/vesc_sim/include/car_config.h"
 #include "car_control_test/Drive.h"
 #include "car_control_test/TargetSpeed.h"
 #include "drive_msgs/gazebo_state_telemetry.h"
+#include "drive_msgs/drive_param.h"
 #include <deque>
+#include <iomanip>
+#include <iostream>
 #include <gazebo_msgs/LinkState.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
@@ -16,10 +18,13 @@
 #pragma GCC diagnostic ignored "-Wdouble-promotion"
 #pragma GCC diagnostic pop
 
-constexpr const char* TOPIC_FOCBOX_SPEED = "/commands/motor/speed";
+constexpr const char* TOPIC_DRIVE_PARAM = "/commands/drive_param";
 constexpr const char* TOPIC_ODOM = "/odom";
 constexpr const char* TOPIC_GAZEBO_STATE_TELEMETRY = "/gazebo/state_telemetry";
 constexpr const char* TOPIC_IMU = "/imu";
+
+const static float CAR_ACCELERATION = 6;
+const static float CAR_DECCELERATION = 5; 
 
 struct Point
 {
@@ -35,6 +40,9 @@ class StraightDrive
 {
     public:
     StraightDrive();
+    /**
+     * @param speed speed in m/s
+     */
     void publishSpeed(double speed);
     float m_speed = 0;
 
@@ -47,8 +55,13 @@ class StraightDrive
     ros::Subscriber m_gazebo_link_states;
     ros::Subscriber m_imu;
 
+    float m_target_rpm = 0;
+    float m_current_rpm = 0;
+
     float linear_acceleration_x;
+    float m_current_speed;
     float m_gazebo_current_car_speed;
+
     Point m_car_pos;
     std::deque<float> m_moving_average_linear_acceleration_x;
     std::deque<float> m_moving_average_wheel_speed;
@@ -57,10 +70,6 @@ class StraightDrive
 
     bool speedAfterDistanceService(car_control_test::TargetSpeed::Request& request,
                                    car_control_test::TargetSpeed::Response& response);
-    float calcBreakingDistance(float current_speed, float target_speed, float distance, float acceleration,
-                               float decceleration);
-    float calcMaxSpeed(float current_speed, float target_speed, float distance, float acceleration,
-                       float decceleration);
 
     void gazeboStateTelemetryCallback(const drive_msgs::gazebo_state_telemetry::ConstPtr& state_telemetry);
     void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_data);
@@ -76,22 +85,45 @@ class StraightDrive
     }
 
     /*
-    speed: m/s
+    current_speed in m/s
+    target_speed in m/s
+    distance in m
+    acceleration in m/s^2
     */
-    int convertSpeedToRpm(float speed)
+    static float calcBreakingDistance(float current_speed, float target_speed, float distance, float acceleration,
+                                            float decceleration)
     {
-        return speed * car_config::TRANSMISSION / car_config::ERPM_TO_SPEED;
+        acceleration = std::abs(acceleration);
+        decceleration = std::abs(decceleration);
+        return (distance * (acceleration / (acceleration + decceleration))) +
+            ((current_speed * current_speed - target_speed * target_speed) / (2 * acceleration + 2 * decceleration));
     }
 
-    float calcTimeLimit(float current_speed, float target_speed, float distance, float acceleration,
-                        float decceleration)
+    static float calcMaxVelocity(float current_speed, float target_speed, float distance, float acceleration, float decceleration) {
+        return std::sqrt((2 * distance * acceleration * decceleration + current_speed * current_speed * decceleration + target_speed * target_speed * acceleration) / (acceleration + decceleration));
+    }
+
+    static float calcAccelerationTime(float current_speed, float target_speed, float distance, float acceleration, float decceleration) {
+        return (calcMaxVelocity(current_speed, target_speed, distance, acceleration, decceleration) - current_speed) / acceleration;
+    }
+
+    static float calcDeccelerationTime(float current_speed, float target_speed, float distance, float acceleration, float decceleration) {
+        return (calcMaxVelocity(current_speed, target_speed, distance, acceleration, decceleration) - target_speed) / decceleration;
+    }
+
+    static float calcTimeLimit(float current_speed, float target_speed, float distance, float acceleration, float decceleration)
     {
-        float max_velocity =
-            std::sqrt((2 * distance * acceleration * decceleration + current_speed * current_speed * decceleration +
-                       target_speed * target_speed * acceleration) /
-                      (acceleration + decceleration));
+        float max_velocity = calcMaxVelocity(current_speed, target_speed, distance, acceleration, decceleration);
         float time_1 = (max_velocity - current_speed) / acceleration;
         float time_2 = (max_velocity - target_speed) / decceleration;
         return time_1 + time_2;
+    }
+
+    static float calcAccelerationSpeed(float starting_speed, float target_speed, float remaining_distance, float distance, float acceleration, float decceleration) {
+        return acceleration*calcAccelerationTime(starting_speed, target_speed, distance-remaining_distance, acceleration, decceleration)+starting_speed;
+    }
+
+    static float calcDeccelerationSpeed(float starting_speed, float target_speed, float remaining_distance, float acceleration, float decceleration) {
+        return acceleration*calcDeccelerationTime(starting_speed, target_speed, remaining_distance, acceleration, decceleration)+target_speed;
     }
 };
--- ros_ws/src/car_control_test/src/drive_straight.cpp
+++ ros_ws/src/car_control_test/src/drive_straight.cpp
@@ -1,10 +1,8 @@
 #include "drive_straight.h"
-#include <iomanip>
-#include <iostream>
 
 StraightDrive::StraightDrive()
 {
-    this->m_straightdrive_publisher = m_node_handle.advertise<std_msgs::Float64>(TOPIC_FOCBOX_SPEED, 1);
+    this->m_straightdrive_publisher = m_node_handle.advertise<drive_msgs::drive_param>(TOPIC_DRIVE_PARAM, 1);
     this->m_drive_with_speed =
         m_node_handle.advertiseService("drive_with_speed", &StraightDrive::drive_with_speed, this);
     this->m_speed_after_distance =
@@ -12,13 +10,6 @@ StraightDrive::StraightDrive()
     this->m_gazebo_link_states = this->m_node_handle.subscribe(TOPIC_GAZEBO_STATE_TELEMETRY, 1,
                                                                &StraightDrive::gazeboStateTelemetryCallback, this);
     this->m_imu = this->m_node_handle.subscribe(TOPIC_IMU, 1, &StraightDrive::imuCallback, this);
-}
-
-void StraightDrive::publishSpeed(double speed)
-{
-    std_msgs::Float64 speed_message;
-    speed_message.data = speed;
-    this->m_straightdrive_publisher.publish(speed_message);
 }
 
 bool StraightDrive::drive_with_speed(car_control_test::Drive::Request& request,
@@ -29,46 +20,30 @@ bool StraightDrive::drive_with_speed(car_control_test::Drive::Request& request,
     return true;
 }
 
-/*
-current_speed in m/s
-target_speed in m/s
-distance in m
-acceleration in m/s^2
-*/
-float StraightDrive::calcBreakingDistance(float current_speed, float target_speed, float distance, float acceleration,
-                                          float decceleration)
-{
-    acceleration = std::abs(acceleration);
-    decceleration = std::abs(decceleration);
-    return (distance * (acceleration / (acceleration + decceleration))) +
-        ((current_speed * current_speed - target_speed * target_speed) / (2 * acceleration + 2 * decceleration));
-}
-
-float StraightDrive::calcMaxSpeed(float current_speed, float target_speed, float distance, float acceleration,
-                                  float decceleration)
-{
-    return std::sqrt((2 * distance * acceleration * decceleration + current_speed * current_speed * decceleration +
-                      target_speed * target_speed * acceleration) /
-                     (acceleration + decceleration));
-}
-
 bool StraightDrive::speedAfterDistanceService(car_control_test::TargetSpeed::Request& request,
                                               car_control_test::TargetSpeed::Response& response)
 {
     float braking_distance =
-        calcBreakingDistance(calcMean(m_moving_average_wheel_speed), request.target_speed, request.distance, 8, 4.5);
+        calcBreakingDistance(calcMean(m_moving_average_wheel_speed), request.target_speed, request.distance, CAR_ACCELERATION, CAR_DECCELERATION);
     response.breaking_distance = braking_distance;
     response.braking_possible = braking_distance < request.distance;
     response.current_speed = calcMean(m_moving_average_wheel_speed);
 
-    float safety_margin = 0.05 * request.distance;
+    float safety_margin;
+    if (request.distance < 5) {
+        safety_margin = 0.05 * request.distance;
+    } else {
+        safety_margin = 0.25;
+    }
 
-    float remain_distance = request.distance - safety_margin;
+    float remain_distance = request.distance;
 
-    float old_speed = 0;
+    float current_speed = calcMean(m_moving_average_wheel_speed);
+    float old_speed = current_speed;
+    float old_speed_wheel = current_speed;
 
     std::cout << "lowest_possible_time: "
-              << calcTimeLimit(calcMean(m_moving_average_wheel_speed), request.target_speed, request.distance, 8, 4.5)
+              << calcTimeLimit(calcMean(m_moving_average_wheel_speed), request.target_speed, request.distance, CAR_ACCELERATION, CAR_DECCELERATION)
               << std::endl;
     std::cout << "______________________________________________________________________________________" << std::endl;
 
@@ -78,60 +53,65 @@ bool StraightDrive::speedAfterDistanceService(car_control_test::TargetSpeed::Req
     float delta_time = 0;
 
     float current_acceleration = 0;
-    float current_acceleration_gazebo = 0;
+    float current_acceleration_wheel = 0;
 
-    float rpm;
+    float speed;
 
     Point car_pos_start = m_car_pos;
-    while (calcMean(m_moving_average_wheel_speed) > request.target_speed * 1.1)
+    while (current_speed > request.target_speed * 1.1 && remain_distance+2 > 0)
     {
         current_time = ros::Time::now();
         delta_time = (current_time.toNSec() - time_old.toNSec()) / 1e9;
         total_time += delta_time;
         time_old = current_time;
-        if (old_speed != 0 && delta_time != 0 && calcMean(m_moving_average_wheel_speed) != old_speed)
+        current_acceleration = calcMean(m_moving_average_linear_acceleration_x);
+        current_speed = current_acceleration*delta_time+old_speed;
+        if (delta_time != 0 && current_speed != old_speed)
         {
-            current_acceleration = calcMean(m_moving_average_linear_acceleration_x);
-            current_acceleration_gazebo = (calcMean(m_moving_average_wheel_speed) - old_speed) / delta_time;
+            current_acceleration_wheel = (calcMean(m_moving_average_wheel_speed) - old_speed_wheel) / delta_time;
+            if (current_acceleration_wheel > -8 && current_acceleration_wheel < 8 && current_acceleration > -8 && current_acceleration < 8) {
+                current_speed = (calcMean(m_moving_average_wheel_speed));
+            }
 
-            std::cout << "current_speed: " << calcMean(m_moving_average_wheel_speed) << ", old_speed: " << old_speed
+            std::cout << "current_speed_wheel: " << calcMean(m_moving_average_wheel_speed) << ", old_speed_wheel: " << old_speed_wheel << ", imu_speed: " << current_speed << ", old_speed: " << old_speed
                       << ", delta_time: " << std::setprecision(9) << delta_time << std::endl;
+            old_speed_wheel = calcMean(m_moving_average_wheel_speed);
+
             remain_distance =
                 remain_distance - (current_acceleration / 2 * (delta_time * delta_time)) - old_speed * delta_time;
-            old_speed = calcMean(m_moving_average_wheel_speed);
+            old_speed = current_speed;
 
-            braking_distance = calcBreakingDistance(calcMean(m_moving_average_wheel_speed), request.target_speed,
-                                                    remain_distance, 8, 4.5);
-            std::cout << "accel: " << current_acceleration << ", r_dist: " << remain_distance + safety_margin
-                      << ", b_dist:" << braking_distance << std::endl;
+            braking_distance = calcBreakingDistance(current_speed, request.target_speed,
+                                                    remain_distance, CAR_ACCELERATION, CAR_DECCELERATION) + safety_margin;
+            std::cout << "accel: " << current_acceleration << ", r_dist: " << remain_distance
+                      << ", b_dist:" << braking_distance - safety_margin << std::endl;
             std::cout << "gazebo_car_speed: " << m_gazebo_current_car_speed
-                      << ", gazebo_acceleration: " << current_acceleration_gazebo << std::endl;
+                      << ", gazebo_acceleration: " << current_acceleration_wheel << std::endl;
             std::cout << "----------------------------------------------------------------------" << std::endl;
 
-            rpm = convertSpeedToRpm(calcMean(m_moving_average_wheel_speed));
-            publishSpeed(std::max<float>(0.0,
-                                         rpm +
-                                             10000 *
-                                                 ((remain_distance - braking_distance) /
-                                                  (std::abs(remain_distance) + std::abs(braking_distance)))));
-        }
-        else if (old_speed == 0)
-        {
-            old_speed = calcMean(m_moving_average_wheel_speed);
+            if (braking_distance < remain_distance) {
+                speed = calcMaxVelocity(current_speed, request.target_speed, remain_distance, CAR_ACCELERATION, CAR_DECCELERATION);
+                std::cout << "vmax: " << speed << std::endl;
+            } else {
+                speed = request.target_speed;
+            }
+            publishSpeed(speed);
+            // publishSpeed(std::max<float>(0.0, rpm + 10000 *((remain_distance - braking_distance) / (std::abs(remain_distance) + std::abs(braking_distance)))));
         }
         for (size_t i = 0; i < 10; i++)
         {
+            publishSpeed(speed);
             ros::spinOnce();
-            ros::Duration(0.01).sleep();
+            ros::Duration(0.005).sleep();
         }
     }
-    m_speed = convertSpeedToRpm(request.target_speed);
+    m_speed = request.target_speed;
     total_time += delta_time;
     remain_distance = remain_distance - (current_acceleration / 2 * (delta_time * delta_time)) - old_speed * delta_time;
     float travelled_distance = std::sqrt((car_pos_start.x - m_car_pos.x) * (car_pos_start.x - m_car_pos.x) +
                                          (car_pos_start.y - m_car_pos.y) * (car_pos_start.y - m_car_pos.y));
     std::cout << "travelled_distance_gazebo: " << travelled_distance
-              << ", travelled_distance_imu: " << request.distance - remain_distance - safety_margin << std::endl;
+              << ", travelled_distance_imu: " << request.distance - remain_distance << std::endl;
     std::cout << "total_time: " << total_time << std::endl;
     std::cout << "______________________________________________________________________________________" << std::endl;
 
@@ -145,6 +125,14 @@ bool StraightDrive::speedAfterDistanceService(car_control_test::TargetSpeed::Req
 
 //     // }
 // }
+
+void StraightDrive::publishSpeed(double speed)
+{
+    drive_msgs::drive_param message;
+    message.velocity = speed;
+    message.angle = 0;
+    this->m_straightdrive_publisher.publish(message);
+}
 
 void StraightDrive::gazeboStateTelemetryCallback(const drive_msgs::gazebo_state_telemetry::ConstPtr& state_telemetry)
 {
@@ -161,7 +149,7 @@ void StraightDrive::gazeboStateTelemetryCallback(const drive_msgs::gazebo_state_
 void StraightDrive::imuCallback(const sensor_msgs::Imu::ConstPtr& imu_data)
 {
     m_moving_average_linear_acceleration_x.push_back(imu_data->linear_acceleration.x);
-    if (m_moving_average_linear_acceleration_x.size() > 5)
+    if (m_moving_average_linear_acceleration_x.size() > 9)
     {
         m_moving_average_linear_acceleration_x.pop_front();
     }
